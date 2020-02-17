c8575a81ed599951d368957047e2553a14874687
AntjeAnderson <55034820+AntjeAnderson@users.noreply.github.com>
2020-02-16 19:20:25 -0600

    Add files via upload

---
+++ GEATests/Eliot_1860.js
@@ -0,0 +1,294 @@
+var data =
+{
+   "type": "FeatureCollection",
+   "features": [
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -0.12775,51.50735 ]
+    },
+    "properties": {
+    "stop_no":1,
+    "name":"London",
+    "year":1860
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 14.26811,40.85179 ]
+    },
+    "properties": {
+    "stop_no":1,
+    "name":"Naples ",
+    "year":1860
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 2.35222,48.85661 ]
+    },
+    "properties": {
+    "stop_no":2,
+    "name":"Paris",
+    "year":1860
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 6.90086,45.25962 ]
+    },
+    "properties": {
+    "stop_no":3,
+    "name":"Mont Cenis",
+    "year":1860
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 7.04845,45.13861 ]
+    },
+    "properties": {
+    "stop_no":4,
+    "name":"Suza",
+    "year":1860
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 7.68686,45.07033 ]
+    },
+    "properties": {
+    "stop_no":5,
+    "name":"Turin",
+    "year":1860
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 8.94625,44.40564 ]
+    },
+    "properties": {
+    "stop_no":6,
+    "name":"Genoa",
+    "year":1860
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 10.40168,43.72283 ]
+    },
+    "properties": {
+    "stop_no":7,
+    "name":"Pisa",
+    "year":1860
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 10.31056,43.54847 ]
+    },
+    "properties": {
+    "stop_no":8,
+    "name":"Livorno",
+    "year":1860
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 11.34261,44.49488 ]
+    },
+    "properties": {
+    "stop_no":9,
+    "name":"Rome ",
+    "year":1860
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 11.25581,43.76956 ]
+    },
+    "properties": {
+    "stop_no":10,
+    "name":"Florence",
+    "year":1860
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 11.34261,44.49488 ]
+    },
+    "properties": {
+    "stop_no":11,
+    "name":"Bologna",
+    "year":1860
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 11.61978,44.83812 ]
+    },
+    "properties": {
+    "stop_no":12,
+    "name":"Ferrara",
+    "year":1860
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 11.87676,45.40643 ]
+    },
+    "properties": {
+    "stop_no":13,
+    "name":"Padua ",
+    "year":1860
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 12.31551,45.44084 ]
+    },
+    "properties": {
+    "stop_no":14,
+    "name":"Venice",
+    "year":1860
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 8.72553,45.63006 ]
+    },
+    "properties": {
+    "stop_no":15,
+    "name":"Milan",
+    "year":1860
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 9.25716,46.01604 ]
+    },
+    "properties": {
+    "stop_no":16,
+    "name":"Lake Como",
+    "year":1860
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 9.25884,45.97824 ]
+    },
+    "properties": {
+    "stop_no":17,
+    "name":"Bellagio",
+    "year":1860
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 9.4012,46.32717 ]
+    },
+    "properties": {
+    "stop_no":18,
+    "name":"Chiavenna ",
+    "year":1860
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 8.54169,47.37688 ]
+    },
+    "properties": {
+    "stop_no":19,
+    "name":"Zurich",
+    "year":1860
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 7.44744,46.94797 ]
+    },
+    "properties": {
+    "stop_no":20,
+    "name":"Bern",
+    "year":1860
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 6.14315,46.20439 ]
+    },
+    "properties": {
+    "stop_no":21,
+    "name":"Geneva",
+    "year":1860
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 2.35222,48.85661 ]
+    },
+    "properties": {
+    "stop_no":22,
+    "name":"Paris",
+    "year":1860
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -0.12775,51.50735 ]
+    },
+    "properties": {
+    "stop_no":23,
+    "name":"London",
+    "year":1860
+    }
+  }
+]
+}

2afcc139badc9ba6057a4af5db3c36913dd659a2
AntjeAnderson <55034820+AntjeAnderson@users.noreply.github.com>
2020-02-20 15:32:12 -0600

    Add files via upload

---
+++ Lab5/Cities3.js
@@ -0,0 +1,652 @@
+var cities =
+{
+   "type": "FeatureCollection",
+   "features": [
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 112.3,31.2 ]
+    },
+    "properties": {
+    "id":1,
+    "city_name":"Hubei",
+    "day1":7153,
+    "day2":9074,
+    "day3":11177,
+    "day4":13522,
+    "day5":16678,
+    "day6":19665,
+    "day7":22112,
+    "day8":24953
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 120.5,29.2 ]
+    },
+    "properties": {
+    "id":2,
+    "city_name":"Zhejiang",
+    "day1":599,
+    "day2":661,
+    "day3":724,
+    "day4":829,
+    "day5":895,
+    "day6":954,
+    "day7":1006,
+    "day8":1075
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 113.5,23.4 ]
+    },
+    "properties": {
+    "id":3,
+    "city_name":"Guangdong",
+    "day1":520,
+    "day2":604,
+    "day3":683,
+    "day4":797,
+    "day5":870,
+    "day6":944,
+    "day7":1018,
+    "day8":1048
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 113.5,33.9 ]
+    },
+    "properties": {
+    "id":4,
+    "city_name":"Henan",
+    "day1":422,
+    "day2":493,
+    "day3":566,
+    "day4":675,
+    "day5":764,
+    "day6":851,
+    "day7":914,
+    "day8":981
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 112.98983,28.11265 ]
+    },
+    "properties": {
+    "id":5,
+    "city_name":"Hunan",
+    "day1":389,
+    "day2":463,
+    "day3":521,
+    "day4":593,
+    "day5":661,
+    "day6":711,
+    "day7":772,
+    "day8":803
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 117,31.83333 ]
+    },
+    "properties": {
+    "id":6,
+    "city_name":"Anhui",
+    "day1":297,
+    "day2":340,
+    "day3":408,
+    "day4":480,
+    "day5":530,
+    "day6":591,
+    "day7":665,
+    "day8":733
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 112.46361,34.68694 ]
+    },
+    "properties": {
+    "id":7,
+    "city_name":"Jiangxi",
+    "day1":286,
+    "day2":333,
+    "day3":391,
+    "day4":476,
+    "day5":548,
+    "day6":600,
+    "day7":661,
+    "day8":698
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 106.56667,29.55833 ]
+    },
+    "properties": {
+    "id":8,
+    "city_name":"Chongqing",
+    "day1":238,
+    "day2":262,
+    "day3":300,
+    "day4":337,
+    "day5":366,
+    "day6":389,
+    "day7":411,
+    "day8":439
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 119.8,32.9 ]
+    },
+    "properties": {
+    "id":9,
+    "city_name":"Jiangsu",
+    "day1":207,
+    "day2":236,
+    "day3":271,
+    "day4":308,
+    "day5":341,
+    "day6":373,
+    "day7":408,
+    "day8":426
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 102.93333,30.13333 ]
+    },
+    "properties": {
+    "id":10,
+    "city_name":"Sichuan",
+    "day1":202,
+    "day2":231,
+    "day3":254,
+    "day4":282,
+    "day5":301,
+    "day6":321,
+    "day7":344,
+    "day8":407
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 118.4,36.4 ]
+    },
+    "properties": {
+    "id":11,
+    "city_name":"Shandong",
+    "day1":202,
+    "day2":225,
+    "day3":246,
+    "day4":270,
+    "day5":298,
+    "day6":343,
+    "day7":379,
+    "day8":363
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 116.38333,39.91667 ]
+    },
+    "properties": {
+    "id":12,
+    "city_name":"Beijing",
+    "day1":156,
+    "day2":183,
+    "day3":212,
+    "day4":228,
+    "day5":253,
+    "day6":274,
+    "day7":297,
+    "day8":315
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 121.4737,31.2304 ]
+    },
+    "properties": {
+    "id":13,
+    "city_name":"Shanghai",
+    "day1":153,
+    "day2":177,
+    "day3":193,
+    "day4":208,
+    "day5":233,
+    "day6":254,
+    "day7":269,
+    "day8":282
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 118.3,25.9 ]
+    },
+    "properties": {
+    "id":14,
+    "city_name":"Fuijan",
+    "day1":144,
+    "day2":159,
+    "day3":179,
+    "day4":194,
+    "day5":205,
+    "day6":215,
+    "day7":224,
+    "day8":281
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 129,48 ]
+    },
+    "properties": {
+    "id":15,
+    "city_name":"Heilongjiang",
+    "day1":101,
+    "day2":116,
+    "day3":128,
+    "day4":155,
+    "day5":190,
+    "day6":227,
+    "day7":277,
+    "day8":239
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 108.4,35.6 ]
+    },
+    "properties": {
+    "id":16,
+    "city_name":"Shaanxi",
+    "day1":100,
+    "day2":111,
+    "day3":127,
+    "day4":142,
+    "day5":165,
+    "day6":173,
+    "day7":184,
+    "day8":195
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 121.18333,24.8 ]
+    },
+    "properties": {
+    "id":17,
+    "city_name":"Guangxi",
+    "day1":96,
+    "day2":104,
+    "day3":118,
+    "day4":139,
+    "day5":150,
+    "day6":168,
+    "day7":172,
+    "day8":195
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 116.7,39.3 ]
+    },
+    "properties": {
+    "id":18,
+    "city_name":"Hebei",
+    "day1":91,
+    "day2":99,
+    "day3":113,
+    "day4":126,
+    "day5":135,
+    "day6":157,
+    "day7":171,
+    "day8":183
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 101.86667,25.05 ]
+    },
+    "properties": {
+    "id":19,
+    "city_name":"Yunnan",
+    "day1":80,
+    "day2":95,
+    "day3":109,
+    "day4":117,
+    "day5":122,
+    "day6":128,
+    "day7":135,
+    "day8":138
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 109.7,19.2 ]
+    },
+    "properties": {
+    "id":20,
+    "city_name":"Hainan",
+    "day1":60,
+    "day2":64,
+    "day3":70,
+    "day4":79,
+    "day5":89,
+    "day6":100,
+    "day7":111,
+    "day8":123
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 122.3,41.1 ]
+    },
+    "properties": {
+    "id":21,
+    "city_name":"Liaoning",
+    "day1":57,
+    "day2":63,
+    "day3":70,
+    "day4":74,
+    "day5":81,
+    "day6":89,
+    "day7":96,
+    "day8":104
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 112.4,37.7 ]
+    },
+    "properties": {
+    "id":22,
+    "city_name":"Shanxi",
+    "day1":47,
+    "day2":56,
+    "day3":51,
+    "day4":74,
+    "day5":81,
+    "day6":90,
+    "day7":94,
+    "day8":99
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 117.18333,39.13333 ]
+    },
+    "properties": {
+    "id":23,
+    "city_name":"Tianjin",
+    "day1":35,
+    "day2":45,
+    "day3":49,
+    "day4":63,
+    "day5":67,
+    "day6":70,
+    "day7":79,
+    "day8":89
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 104.2861,38.7518 ]
+    },
+    "properties": {
+    "id":24,
+    "city_name":"Gansu",
+    "day1":34,
+    "day2":40,
+    "day3":66,
+    "day4":56,
+    "day5":57,
+    "day6":62,
+    "day7":77,
+    "day8":81
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 106.83333,28.83333 ]
+    },
+    "properties": {
+    "id":25,
+    "city_name":"Guizhou",
+    "day1":29,
+    "day2":38,
+    "day3":46,
+    "day4":56,
+    "day5":64,
+    "day6":69,
+    "day7":70,
+    "day8":71
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 126.2,43.7 ]
+    },
+    "properties": {
+    "id":26,
+    "city_name":"Jilin",
+    "day1":26,
+    "day2":28,
+    "day3":33,
+    "day4":42,
+    "day5":54,
+    "day6":59,
+    "day7":65,
+    "day8":69
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 113,44 ]
+    },
+    "properties": {
+    "id":27,
+    "city_name":"Inner Mongolia",
+    "day1":23,
+    "day2":26,
+    "day3":31,
+    "day4":37,
+    "day5":42,
+    "day6":46,
+    "day7":49,
+    "day8":50
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 106.1581,37.1984 ]
+    },
+    "properties": {
+    "id":28,
+    "city_name":"Ningxia",
+    "day1":18,
+    "day2":23,
+    "day3":31,
+    "day4":34,
+    "day5":34,
+    "day6":40,
+    "day7":43,
+    "day8":45
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 87.5396,42.5246 ]
+    },
+    "properties": {
+    "id":29,
+    "city_name":"Xinjiang",
+    "day1":17,
+    "day2":21,
+    "day3":24,
+    "day4":29,
+    "day5":32,
+    "day6":36,
+    "day7":39,
+    "day8":42
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 114.1694,22.3193 ]
+    },
+    "properties": {
+    "id":30,
+    "city_name":"Hong Kong SAR",
+    "day1":13,
+    "day2":14,
+    "day3":15,
+    "day4":15,
+    "day5":18,
+    "day6":21,
+    "day7":24,
+    "day8":26
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 96,35 ]
+    },
+    "properties": {
+    "id":31,
+    "city_name":"Qinghai",
+    "day1":10,
+    "day2":10,
+    "day3":13,
+    "day4":15,
+    "day5":17,
+    "day6":18,
+    "day7":18,
+    "day8":18
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 121.51667,25.06667 ]
+    },
+    "properties": {
+    "id":32,
+    "city_name":"Taipei",
+    "day1":8,
+    "day2":9,
+    "day3":10,
+    "day4":10,
+    "day5":11,
+    "day6":11,
+    "day7":16,
+    "day8":16
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 113.55,22.16667 ]
+    },
+    "properties": {
+    "id":33,
+    "city_name":"Macao SAR ",
+    "day1":7,
+    "day2":7,
+    "day3":8,
+    "day4":8,
+    "day5":10,
+    "day6":10,
+    "day7":10,
+    "day8":10
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 88.88059,29.26687 ]
+    },
+    "properties": {
+    "id":34,
+    "city_name":"Xizang",
+    "day1":1,
+    "day2":1,
+    "day3":1,
+    "day4":1,
+    "day5":1,
+    "day6":1,
+    "day7":1,
+    "day8":1
+    }
+  }
+]
+};

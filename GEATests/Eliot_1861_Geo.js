2ff84c1ecc39ee859e5d2db27834b2af7ad8e96c
AntjeAnderson <55034820+AntjeAnderson@users.noreply.github.com>
2020-04-16 13:21:29 -0500

    Add files via upload

---
+++ GEATests/Eliot_1861_Geo.js
@@ -0,0 +1,28 @@
+var trythis = {
+"type": "FeatureCollection",
+"name": "Eliot_1861_Geo",
+"features": [
+{ "type": "Feature", "properties": { "stop_no": 1, "name": "London", "longitude": -0.12775, "latitude": 51.50735, "year": 1861 }, "geometry": null },
+{ "type": "Feature", "properties": { "stop_no": 2, "name": "Paris", "longitude": 2.35222, "latitude": 48.85661, "year": 1861 }, "geometry": null },
+{ "type": "Feature", "properties": { "stop_no": 3, "name": "Avignon ", "longitude": 4.80552, "latitude": 43.94931, "year": 1861 }, "geometry": null },
+{ "type": "Feature", "properties": { "stop_no": 4, "name": "Toulon", "longitude": 5.92799, "latitude": 43.12422, "year": 1861 }, "geometry": null },
+{ "type": "Feature", "properties": { "stop_no": 5, "name": "Vidaubon", "longitude": 6.43161, "latitude": 43.42686, "year": 1861 }, "geometry": null },
+{ "type": "Feature", "properties": { "stop_no": 6, "name": "Nice", "longitude": 7.26195, "latitude": 43.71017, "year": 1861 }, "geometry": null },
+{ "type": "Feature", "properties": { "stop_no": 7, "name": "Cannes", "longitude": 7.01736, "latitude": 43.55284, "year": 1861 }, "geometry": null },
+{ "type": "Feature", "properties": { "stop_no": 8, "name": "Genoa", "longitude": 8.94625, "latitude": 44.40564, "year": 1861 }, "geometry": null },
+{ "type": "Feature", "properties": { "stop_no": 9, "name": "Pisa", "longitude": 10.40168, "latitude": 43.72283, "year": 1861 }, "geometry": null },
+{ "type": "Feature", "properties": { "stop_no": 1, "name": "Florence ", "longitude": 11.25581, "latitude": 43.76956, "year": 1861 }, "geometry": null },
+{ "type": "Feature", "properties": { "stop_no": 11, "name": "Pistoia", "longitude": 10.90785, "latitude": 43.93034, "year": 1861 }, "geometry": null },
+{ "type": "Feature", "properties": { "stop_no": 12, "name": "Camaldoli monastery ", "longitude": 11.82037, "latitude": 43.79449, "year": 1861 }, "geometry": null },
+{ "type": "Feature", "properties": { "stop_no": 13, "name": "La Vernia monastery ", "longitude": 11.93139, "latitude": 43.70724, "year": 1861 }, "geometry": null },
+{ "type": "Feature", "properties": { "stop_no": 14, "name": "Florence", "longitude": 11.25581, "latitude": 43.76956, "year": 1861 }, "geometry": null },
+{ "type": "Feature", "properties": { "stop_no": 15, "name": "Leghorn\/Livorno", "longitude": 10.31056, "latitude": 43.54847, "year": 1861 }, "geometry": null },
+{ "type": "Feature", "properties": { "stop_no": 16, "name": "Genoa", "longitude": 8.94625, "latitude": 44.40564, "year": 1861 }, "geometry": null },
+{ "type": "Feature", "properties": { "stop_no": 17, "name": "Lago Maggiore", "longitude": 8.65325, "latitude": 45.96762, "year": 1861 }, "geometry": null },
+{ "type": "Feature", "properties": { "stop_no": 18, "name": "Lake Lucerne", "longitude": 8.3093, "latitude": 47.05016, "year": 1861 }, "geometry": null },
+{ "type": "Feature", "properties": { "stop_no": 19, "name": "Bern", "longitude": 7.44744, "latitude": 46.94797, "year": 1861 }, "geometry": null },
+{ "type": "Feature", "properties": { "stop_no": 20, "name": "Basel", "longitude": 7.58857, "latitude": 47.55959, "year": 1861 }, "geometry": null },
+{ "type": "Feature", "properties": { "stop_no": 21, "name": "Paris ", "longitude": 2.35222, "latitude": 48.85661, "year": 1861 }, "geometry": null },
+{ "type": "Feature", "properties": { "stop_no": 22, "name": "London ", "longitude": -0.12775, "latitude": 51.50735, "year": 1861 }, "geometry": null }
+]
+}

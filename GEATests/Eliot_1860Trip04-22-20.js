1f626aade59125e588d0ae8bc36e232b0d6b5276
AntjeAnderson <55034820+AntjeAnderson@users.noreply.github.com>
2020-04-22 13:43:25 -0500

    Add files via upload

---
+++ GEATests/Eliot_1860Trip04-22-20.js
@@ -0,0 +1,341 @@
+var Eliot_1860Trip = {
+   "type": "FeatureCollection",
+   "features": [
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -0.12775,51.50735 ]
+    },
+    "properties": {
+    "stop_no":1,
+    "name":"London",
+    "year":1860,
+    "date":"March 1860",
+    "Description":"GE and GHL left London for their Italian trip at the end of March. "
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 2.35222,48.85661 ]
+    },
+    "properties": {
+    "stop_no":2,
+    "name":"Paris",
+    "year":1860,
+    "date":"03/24/1860",
+    "Description":"GE and GHL saw the Louvre Notre Dame and Ste. Chapelle."
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 6.90086,45.25962 ]
+    },
+    "properties": {
+    "stop_no":3,
+    "name":"Mont Cenis",
+    "year":1860,
+    "date":"March 1860",
+    "Description":"The trip over Mont Cenis Pass, first by diligence (carriage) and then by sledge into Italy was a common access route for travelers."
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 7.04845,45.13861 ]
+    },
+    "properties": {
+    "stop_no":4,
+    "name":"Suza",
+    "year":1860,
+    "date":"March 1860",
+    "Description":"From Suza, they took the train to Turin. "
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 7.68686,45.07033 ]
+    },
+    "properties": {
+    "stop_no":5,
+    "name":"Turin",
+    "year":1860,
+    "date":"March 1860",
+    "Description":"Here, they saw signs of the Italian struggle for unification (a major treaty had just been signed days before); at the railway station, they caught a glimpse of Italy's future prime Minister, Count Cavour."
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 8.94625,44.40564 ]
+    },
+    "properties": {
+    "stop_no":6,
+    "name":"Genoa",
+    "year":1860,
+    "date":"March 1860",
+    "Description":"GE had briefly visited this city in 1849--the only Italian city on her trip with the Brays. "
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 10.40168,43.72283 ]
+    },
+    "properties": {
+    "stop_no":7,
+    "name":"Pisa",
+    "year":1860,
+    "date":"March 1860",
+    "Description":""
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 10.31056,43.54847 ]
+    },
+    "properties": {
+    "stop_no":8,
+    "name":"Livorno",
+    "year":1860,
+    "date":"March 1860",
+    "Description":""
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 12.49636,41.90278 ]
+    },
+    "properties": {
+    "stop_no":9,
+    "name":"Rome ",
+    "year":1860,
+    "date":"04/01/1860",
+    "Description":"<p>Eliot and Lewes spent almost a month in Rome on this trip (April 1-29), exploring its famous sites and art museums with a focus on ancient Rome and on the Renaissance. In her &quot;Recollections of Italy&quot; Eliot records that she initially disliked Rome, but she became increasingly fascinated with its sights. Some of the early disappointment with Rome was to be integrated into <em>Middlemarch</em>, as part of the famous description of Dorothea&#39;s disenchantment with the city on her honeymoon.</p>"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 14.26811,40.85179 ]
+    },
+    "properties": {
+    "stop_no":10,
+    "name":"Naples ",
+    "year":1860,
+    "date":"04/30/1860",
+    "Description":"The two weeks here included excursions to Pompeii and environs."
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 11.25581,43.76956 ]
+    },
+    "properties": {
+    "stop_no":11,
+    "name":"Florence",
+    "year":1860,
+    "date":"05/17/1860",
+    "Description":"<p>On their 1860 trip, Eliot and Lewes spent about two weeks in mid-May exploring the art and history of Florence, staying at the Pension Suisse in the Via Tornabuoni, with excursions to Fiesole, Siena, and other sites. On May 21, Eliot decided to write a historical novel set in Renaissance Florence in the 1490s--the novel that became <em>Romola</em> in 1863.</p><p><a href=\"http://georgeeliotartworld.com/files/original/a5dc6f4f55f0486b72bccda95e1926d8.jpg\" target=\"_blank\"><img alt=\"\" src=\"http://georgeeliotartworld.com/files/original/a5dc6f4f55f0486b72bccda95e1926d8.jpg\" style=\"width: 100%; height: 100%;\" /></a></p><p><strong>Zoom in on Florence for further information on some of the most important sites that Eliot visited and commented on in her &quot;Recollections of Italy. 1860.&quot;</p>"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 11.34261,44.49488 ]
+    },
+    "properties": {
+    "stop_no":12,
+    "name":"Bologna",
+    "year":1860,
+    "date":"06/01/1860",
+    "Description":""
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 11.61978,44.83812 ]
+    },
+    "properties": {
+    "stop_no":13,
+    "name":"Ferrara",
+    "year":1860,
+    "date":"[06/02/1860]",
+    "Description":""
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 11.87676,45.40643 ]
+    },
+    "properties": {
+    "stop_no":14,
+    "name":"Padua ",
+    "year":1860,
+    "date":"[06/03/1860]",
+    "Description":"GE highlighted the famous Arena chapel, with Giotto's frescoes, during the short stay here."
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 12.31551,45.44084 ]
+    },
+    "properties": {
+    "stop_no":15,
+    "name":"Venice",
+    "year":1860,
+    "date":"06/04/1860",
+    "Description":"The week here was their last longer stay in Italy; St. Mark and the paintings of Tintorett were the highlights. "
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 8.72553,45.63006 ]
+    },
+    "properties": {
+    "stop_no":16,
+    "name":"Milan",
+    "year":1860,
+    "date":"06/12/1860",
+    "Description":"The Brera and the Ambrosian library were the big sightseeing stops. "
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 9.25716,46.01604 ]
+    },
+    "properties": {
+    "stop_no":17,
+    "name":"Lake Como",
+    "year":1860,
+    "date":"June 1860",
+    "Description":"The stops along the lakes in Northern Italy--Lake Como, and then Bellagio, were common \"spa\" stops for visitors. "
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 9.25884,45.97824 ]
+    },
+    "properties": {
+    "stop_no":18,
+    "name":"Bellagio",
+    "year":1860,
+    "date":"June 1860",
+    "Description":""
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 9.4012,46.32717 ]
+    },
+    "properties": {
+    "stop_no":19,
+    "name":"Chiavenna ",
+    "year":1860,
+    "date":"June 1860",
+    "Description":"From here, their last Italian stop, GE and GHL took the Splügen Pass across the Alps to Switzerland. "
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 8.54169,47.37688 ]
+    },
+    "properties": {
+    "stop_no":20,
+    "name":"Zurich",
+    "year":1860,
+    "date":"June 1860",
+    "Description":"GE and GHL took 2 weeks to return home, with several stays in Switzerland and France, beginning here. "
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 7.44744,46.94797 ]
+    },
+    "properties": {
+    "stop_no":21,
+    "name":"Bern",
+    "year":1860,
+    "date":"June 1860",
+    "Description":"While in Bern, GHL visited Hofwyl, where his sons were at a Swiss boarding school. "
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 6.14315,46.20439 ]
+    },
+    "properties": {
+    "stop_no":22,
+    "name":"Geneva",
+    "year":1860,
+    "date":"June 1860",
+    "Description":""
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 2.35222,48.85661 ]
+    },
+    "properties": {
+    "stop_no":23,
+    "name":"Paris",
+    "year":1860,
+    "date":"June 1860",
+    "Description":""
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -0.12775,51.50735 ]
+    },
+    "properties": {
+    "stop_no":24,
+    "name":"London",
+    "year":1860,
+    "date":"07/01/1860",
+    "Description":"After over three months (all of April, May, and June), GE and GHL returned to London on July 1. "
+    }
+  }
+]
+};

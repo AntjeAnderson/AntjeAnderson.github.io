563a723e1c5e82ef4941dae54c3a49d5849e7152
AntjeAnderson <55034820+AntjeAnderson@users.noreply.github.com>
2020-04-25 13:41:16 -0500

    Add files via upload

---
+++ GEAFinal/Eliot_Cities1864.js
@@ -0,0 +1,243 @@
+var Eliot_Cities1864 = {
+   "type": "FeatureCollection",
+   "features": [
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -0.12775,51.50735 ]
+    },
+    "properties": {
+    "stop_no":1,
+    "name":"London",
+    "year":1864,
+    "date":"1864-05-04",
+    "Description":"Eliot, Lewes, and Burton left from Charing Cross at 7:30 in the morning and were in Paris by nightfall."
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 2.35222,48.85661 ]
+    },
+    "properties": {
+    "stop_no":2,
+    "name":"Paris",
+    "year":1864,
+    "date":"1865-05-06",
+    "Description":"Visits to Notre Dame and the Louvre."
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 6.90086,45.25962 ]
+    },
+    "properties": {
+    "stop_no":3,
+    "name":"Mont Cenis",
+    "year":1864,
+    "date":"1864-05-07",
+    "Description":"As in 1860, they crossed the Alps across Mont Cenis Pass to Suza."
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 7.68686,45.07033 ]
+    },
+    "properties": {
+    "stop_no":4,
+    "name":"Turin",
+    "year":1864,
+    "date":"1864-05-08",
+    "Description":"Visit to the Picture Gallery and the opera."
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 9.19,45.4642 ]
+    },
+    "properties": {
+    "stop_no":5,
+    "name":"Milan",
+    "year":1864,
+    "date":"1864-05-09",
+    "Description":"Visits to the Brera gallery and the Ambrosian manuscript library."
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 12.31551,45.44084 ]
+    },
+    "properties": {
+    "stop_no":8,
+    "name":" Venice",
+    "year":1864,
+    "date":"1864-05-12",
+    "Description":"Eliot, Lewes and Burton spend over two weeks in Venice. <br> Visits to St. Mark's, the Ducal Palace, the Accademia and the Frari, and many churches.<br> Side trips to the surrounding areas, including Murano."
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 12.31551,45.44084 ]
+    },
+    "properties": {
+    "stop_no":9,
+    "name":"Padua",
+    "year":1864,
+    "date":"1864-05-28",
+    "Description":"Eliot and Lewes visit Padua on their own, including the Emeritani Chapel and Arena chapels."
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 10.99162,45.43838 ]
+    },
+    "properties": {
+    "stop_no":10,
+    "name":"Verona",
+    "year":1864,
+    "date":"1864-05-31",
+    "Description":"Eliot and Lewes visit more churches and other sights."
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 10.2118,45.54155 ]
+    },
+    "properties": {
+    "stop_no":11,
+    "name":"Brescia",
+    "year":1864,
+    "date":"1864-06-03",
+    "Description":"Visits to many churches and gallleries, including the Museo Civico."
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 8.72553,45.63006 ]
+    },
+    "properties": {
+    "stop_no":12,
+    "name":"Milan",
+    "year":1864,
+    "date":"1864-06-04",
+    "Description":"Here, Eliot and Lewes reunite with Burton. The Brera galleries are again the highlight."
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 9.25716,46.01604 ]
+    },
+    "properties": {
+    "stop_no":13,
+    "name":"Lake Como",
+    "year":1864,
+    "date":"1864-06-09",
+    "Description":"From here, Eliot, Lewes and Burton went on to Lake Lugano, and continued by steamboat. "
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 9.02441,46.19462 ]
+    },
+    "properties": {
+    "stop_no":14,
+    "name":"Bellinzona",
+    "year":1864,
+    "date":"1864-06-11",
+    "Description":""
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 8.61237,46.5286 ]
+    },
+    "properties": {
+    "stop_no":15,
+    "name":"Airolo",
+    "year":1864,
+    "date":"1864-06-12",
+    "Description":"From here, Eliot, Lewes and Burton crossed the Alps at St. Gotthard int Switzerland."
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 7.44744,46.94797 ]
+    },
+    "properties": {
+    "stop_no":16,
+    "name":"Lucerne",
+    "year":1864,
+    "date":"1864-06-13",
+    "Description":""
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 7.58857,47.55959 ]
+    },
+    "properties": {
+    "stop_no":17,
+    "name":"Basel",
+    "year":1864,
+    "date":"1864-06-15",
+    "Description":""
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 2.35222,48.85661 ]
+    },
+    "properties": {
+    "stop_no":18,
+    "name":"Paris",
+    "year":1864,
+    "date":"1864-06-16",
+    "Description":""
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -0.12775,51.50735 ]
+    },
+    "properties": {
+    "stop_no":19,
+    "name":"London",
+    "year":1864,
+    "date":"1864-06-20",
+    "Description":"Eliot writes to a friend: \"You can imagine that London does not look pretty after such a journey\" (<it>Letters</it> 4:154)."
+    }
+  }
+]
+};

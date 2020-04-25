607a5a6e9a8194ff1be80df7ac7174a7ab6a688d
AntjeAnderson <55034820+AntjeAnderson@users.noreply.github.com>
2020-04-25 09:32:26 -0500

    Add files via upload

---
+++ GEATests/Eliot1861_PLUS.js
@@ -0,0 +1,313 @@
+var Eliot_1861Trip = {
+   "type": "FeatureCollection",
+   "features": [
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -0.12775,51.50735 ]
+    },
+    "properties": {
+    "stop_no":1,
+    "name":"London",
+    "year":1861,
+    "date":"1861-04-19",
+    "Description":""
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 2.35222,48.85661 ]
+    },
+    "properties": {
+    "stop_no":2,
+    "name":"Paris",
+    "year":1861,
+    "date":"[1861-04-20]",
+    "Description":""
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 4.80552,43.94931 ]
+    },
+    "properties": {
+    "stop_no":3,
+    "name":"Avignon",
+    "year":1861,
+    "date":"1861-04-21",
+    "Description":""
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 5.92799,43.12422 ]
+    },
+    "properties": {
+    "stop_no":4,
+    "name":"Toulon",
+    "year":1861,
+    "date":"[1861-04-22]",
+    "Description":""
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 6.43161,43.42686 ]
+    },
+    "properties": {
+    "stop_no":5,
+    "name":"Vidaubon",
+    "year":1861,
+    "date":"[1861-04-24]",
+    "Description":""
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 7.26195,43.71017 ]
+    },
+    "properties": {
+    "stop_no":6,
+    "name":"Nice",
+    "year":1861,
+    "date":"1861-04-25",
+    "Description":""
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 7.01736,43.55284 ]
+    },
+    "properties": {
+    "stop_no":7,
+    "name":"Cannes",
+    "year":1861,
+    "date":"[1861-04-25]",
+    "Description":""
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 8.94625,44.40564 ]
+    },
+    "properties": {
+    "stop_no":8,
+    "name":"Genoa",
+    "year":1861,
+    "date":"1861-04-26",
+    "Description":""
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 10.40168,43.72283 ]
+    },
+    "properties": {
+    "stop_no":9,
+    "name":"Pisa",
+    "year":1861,
+    "date":"[1861-05-01]",
+    "Description":""
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 11.25581,43.76956 ]
+    },
+    "properties": {
+    "stop_no":10,
+    "name":"Florence ",
+    "year":1861,
+    "date":"1861-05-04",
+    "Description":"The main destination during this trip, with a focus on library research and going to museums <br> in preparation to write <it>Romola</it>. In her journal, Eliot wondered: <br>\"thirty-four days of precious time spent there: will it all be in vain?\" <br> (<it>Journals </it> 89)"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 10.90785,43.93034 ]
+    },
+    "properties": {
+    "stop_no":11,
+    "name":"Pistoia",
+    "year":1861,
+    "date":"1861-05-28",
+    "Description":"Eliot and Lewes included Pistoia among several excursions from Florence. "
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 11.82037,43.79449 ]
+    },
+    "properties": {
+    "stop_no":12,
+    "name":"Camaldoli monastery",
+    "year":1861,
+    "date":"1861-06-03",
+    "Description":"This visit was a part of a 5-day excursion from Florence with Thomas Trollope. <br> Eliot did not have access to the inside of the Monastery. "
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 11.93139,43.70724 ]
+    },
+    "properties": {
+    "stop_no":13,
+    "name":"La Vernia monastery",
+    "year":1861,
+    "date":"1861-06-05",
+    "Description":"This visit was a part of a 5-day excursion from Florence with Thomas Trollope."
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 11.25581,43.76956 ]
+    },
+    "properties": {
+    "stop_no":14,
+    "name":"Florence",
+    "year":1861,
+    "date":"1861-06-06",
+    "Description":""
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 10.31056,43.54847 ]
+    },
+    "properties": {
+    "stop_no":15,
+    "name":"Leghorn/Livorno",
+    "year":1861,
+    "date":"1861-06-07",
+    "Description":""
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 8.94625,44.40564 ]
+    },
+    "properties": {
+    "stop_no":16,
+    "name":"Genoa",
+    "year":1861,
+    "date":"[1861-06-08]",
+    "Description":""
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 8.65325,45.96762 ]
+    },
+    "properties": {
+    "stop_no":17,
+    "name":"Lago Maggiore",
+    "year":1861,
+    "date":"[1861-06-09]",
+    "Description":"After a day here, they crossed the Alps on the St. Gotthard Pass."
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 8.3093,47.05016 ]
+    },
+    "properties": {
+    "stop_no":18,
+    "name":"Lake Lucerne",
+    "year":1861,
+    "date":"[1861-06-10]",
+    "Description":""
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 7.44744,46.94797 ]
+    },
+    "properties": {
+    "stop_no":19,
+    "name":"Bern",
+    "year":1861,
+    "date":"1861-06-11",
+    "Description":"Lewes' son Bertie came from his school in Hofwyl to stay with them in Bern. "
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 7.58857,47.55959 ]
+    },
+    "properties": {
+    "stop_no":20,
+    "name":"Basel",
+    "year":1861,
+    "date":"[1861-06-12]",
+    "Description":""
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 2.35222,48.85661 ]
+    },
+    "properties": {
+    "stop_no":21,
+    "name":"Paris",
+    "year":1861,
+    "date":"[1861-06-13]",
+    "Description":"The overnight in Paris did include a visit to the Louvre and to the theater. "
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -0.12775,51.50735 ]
+    },
+    "properties": {
+    "stop_no":22,
+    "name":"London",
+    "year":1861,
+    "date":"1861-06-14",
+    "Description":""
+    }
+  }
+]
+};

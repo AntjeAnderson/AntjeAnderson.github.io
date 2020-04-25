08a4fde78876ae8ce16538e6d257536582095571
AntjeAnderson <55034820+AntjeAnderson@users.noreply.github.com>
2020-04-25 11:52:43 -0500

    Add files via upload

---
+++ GEATests/Eliot1869_Trip.js
@@ -0,0 +1,313 @@
+var Eliot_1869Trip = {
+   "type": "FeatureCollection",
+   "features": [
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -0.12775,51.50735 ]
+    },
+    "properties": {
+    "stop_no":1,
+    "name":"London",
+    "year":1869,
+    "date":"1869-03-03",
+    "Description":"Start in rough weather, but they get to Paris by 9 pm"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 2.35222,48.85661 ]
+    },
+    "properties": {
+    "stop_no":2,
+    "name":"Paris",
+    "year":1869,
+    "date":"1869-03-04",
+    "Description":"Visit to the Louvre, but skip the theater. "
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 4.83565,45.76404 ]
+    },
+    "properties": {
+    "stop_no":3,
+    "name":"Lyon",
+    "year":1869,
+    "date":"1869-03-05",
+    "Description":"Visit to the Cathedral and the theater."
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 4.80552,43.94931 ]
+    },
+    "properties": {
+    "stop_no":4,
+    "name":"Avignon",
+    "year":1869,
+    "date":"1869-03-07",
+    "Description":""
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 5.36977,43.29648 ]
+    },
+    "properties": {
+    "stop_no":5,
+    "name":"Marseilles",
+    "year":1869,
+    "date":"1869-03-08",
+    "Description":""
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 7.26195,43.71017 ]
+    },
+    "properties": {
+    "stop_no":6,
+    "name":"Nice",
+    "year":1869,
+    "date":"1869-03-09",
+    "Description":"They stayed here for several days of patchy weather."
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 7.5,43.775 ]
+    },
+    "properties": {
+    "stop_no":7,
+    "name":"Menton ",
+    "year":1869,
+    "date":"1869-03-14",
+    "Description":"Their last French stop before crossing into Italy."
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 8.94625,44.40564 ]
+    },
+    "properties": {
+    "stop_no":8,
+    "name":"Genoa",
+    "year":1869,
+    "date":"1869-03-16",
+    "Description":"Lewes notes that Genoa was \"not interesting on a 3rd visit.\" <br> They visit several locations in the vicinity."
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 10.40168,43.72283 ]
+    },
+    "properties": {
+    "stop_no":9,
+    "name":"Pisa",
+    "year":1869,
+    "date":"1869-03-19",
+    "Description":"Visit to the Cathedral, Baptistry, and the Leaning Tower. "
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 11.25581,43.76956 ]
+    },
+    "properties": {
+    "stop_no":10,
+    "name":"Florence",
+    "year":1869,
+    "date":"1869-03-20",
+    "Description":"For this first 5-day stay in Florence with Thomas Trollope, Eliot was ill and barely left the house. <br> "
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 14.26811,40.85179 ]
+    },
+    "properties": {
+    "stop_no":11,
+    "name":"Naples ",
+    "year":1869,
+    "date":"1869-03-26",
+    "Description":"A week-long visit including the main museums and sights. From here, they also visited Pompeii on March 29 by railway."
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 12.49636,41.90278 ]
+    },
+    "properties": {
+    "stop_no":12,
+    "name":"Rome",
+    "year":1869,
+    "date":"1869-04-04",
+    "Description":"During their 2 1/2 weeks, they visited old favorites in Rome, including the Vatican and St. Peter's several times. <br> On April 18, they meet John Cross for the first time. <br> "
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 12.6175,43.075833 ]
+    },
+    "properties": {
+    "stop_no":13,
+    "name":"Assisi",
+    "year":1869,
+    "date":"1869-04-21",
+    "Description":"This was Eliot and Lewes' first trip to Assisi, via Narni and Foligno."
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 12.388889,43.112222 ]
+    },
+    "properties": {
+    "stop_no":14,
+    "name":"Perugia",
+    "year":1869,
+    "date":"1869-04-22",
+    "Description":"They visited the Sala de Cambio and continued to Florence that evening."
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 11.25581,43.76956 ]
+    },
+    "properties": {
+    "stop_no":15,
+    "name":"Florence",
+    "year":1869,
+    "date":"1869-04-23",
+    "Description":"For their second 5-day stay, they again stayed with the Trollopes at the Villa Ricorboli on the outskirts of Florence. <br> They revsited many museums, and Eliot went to the new Museo di San Marco on April 24 and 26, <br> seeing the frescoes by Fra Angelico that had been off limits to her as a woman in 1860 and 1861. "
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 12.20352,44.41835 ]
+    },
+    "properties": {
+    "stop_no":16,
+    "name":"Ravenna",
+    "year":1869,
+    "date":"1869-04-27",
+    "Description":"They came her via Bologna, visiting for the first time; Lewes notes how much he dislikes the early Christian mosaics. "
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 10.99162,45.43838 ]
+    },
+    "properties": {
+    "stop_no":17,
+    "name":"Verona",
+    "year":1869,
+    "date":"1869-04-29",
+    "Description":"They revisited the Arena and other sights, and then departed for their return journey. "
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 11.50606,47.00178 ]
+    },
+    "properties": {
+    "stop_no":18,
+    "name":"Brenner Pass",
+    "year":1869,
+    "date":"1869-04-30",
+    "Description":"This time, Eliot and Lewes took the Eastern route out of Italy to Germany across the Brenner Pass. "
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 11.50606,47.00178 ]
+    },
+    "properties": {
+    "stop_no":19,
+    "name":"Munich",
+    "year":1869,
+    "date":"1869-05-01",
+    "Description":"They visited the New Pinakothek, the Glyptothek, and the park (Englischer Garten). "
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 2.35222,48.85661 ]
+    },
+    "properties": {
+    "stop_no":20,
+    "name":"Paris ",
+    "year":1869,
+    "date":"1869-05-02",
+    "Description":"They pushed on to Paris from Munich (via Strasburg) on an overnight train."
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 1.85868,50.95129 ]
+    },
+    "properties": {
+    "stop_no":21,
+    "name":"Calais ",
+    "year":1869,
+    "date":"1869-05-03",
+    "Description":"Brief overnight in Calais."
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -0.12775,51.50735 ]
+    },
+    "properties": {
+    "stop_no":22,
+    "name":"London",
+    "year":1869,
+    "date":"1869-05-06",
+    "Description":"Soon after their return home, Lewes' son Thornie joins them from South Africa, severely ill (he dies in October). "
+    }
+  }
+]
+};
